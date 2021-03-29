0e769c63575260475a363e67542565a8c5f0787f
Farid Rashidi <farid.rsh@gmail.com>
2021-03-28 21:16:33 -0400

    trisicell module

--- trisicell/__init__.py
+++ trisicell/__init__.py
@@ -1,10 +1,13 @@
+import trisicell.datasets as datasets
 import trisicell.io as io
+import trisicell.logging as logg
 import trisicell.pl as pl
 import trisicell.pp as pp
+import trisicell.settings as settings
 import trisicell.tl as tl
 import trisicell.ul as ul
 
 __author__ = ", ".join(["Farid Rashidi"])
 __maintainer__ = ", ".join(["Farid Rashidi"])
 __email__ = ", ".join(["farid.rsh@gmail.com"])
-__version__ = "0.0.1"
+__version__ = "0.0.0"

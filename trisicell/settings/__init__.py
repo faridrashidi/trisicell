eead3ae1a1bde4606501f219fb49bf50997bdba9
Farid Rashidi <farid.rsh@gmail.com>
2021-03-28 21:19:26 -0400

    trisicell settings

---
+++ trisicell/settings/__init__.py
@@ -0,0 +1,1 @@
+from trisicell.settings._settings import *
---
+++ trisicell/settings/_settings.py
@@ -0,0 +1,5 @@
+verbosity = 3
+"""Verbosity level (0=errors, 1=warnings, 2=info, 3=hints)"""
+
+logfile = ""
+"""Name of logfile. By default is set to '' and writes to standard output."""

0c1ee61a51f184942cac3bc4b1abe9401c750918
Farid Rashidi <farid.rsh@gmail.com>
2021-04-05 19:31:16 -0400

    update consensus test

--- tests/test_consensus.py
+++ tests/test_consensus.py
@@ -1,4 +1,3 @@
-import networkx as nx
 import pytest
 from _helpers import *
 
@@ -7,39 +6,39 @@ import trisicell as tsc
 
 class TestConsensus:
     def test_consensus_1(self):
+        # result in biorxiv.figs18b
         f1 = tsc.ul.get_file("trisicell.datasets/test/consensus/biorxiv.fig3b.CFMatrix")
         f2 = tsc.ul.get_file(
             "trisicell.datasets/test/consensus/biorxiv.figs18a.CFMatrix"
         )
         sc1 = tsc.io.read(f1)
         sc2 = tsc.io.read(f2)
-        cnt_tree1, cnt_tree2 = tsc.tl.consensus_run(sc1, sc2)
-        ii = nx.is_isomorphic(cnt_tree1, cnt_tree2)  # result in biorxiv.figs18b
-        assert ii == True
+        final_tree = tsc.tl.consensus_tree(sc1, sc2)
+        assert len(final_tree.nodes) == 19
 
     def test_consensus_2(self):
+        # result in recomb.fig1c
         f1 = tsc.ul.get_file("trisicell.datasets/test/consensus/recomb.fig1a.CFMatrix")
         f2 = tsc.ul.get_file("trisicell.datasets/test/consensus/recomb.fig1b.CFMatrix")
         sc1 = tsc.io.read(f1)
         sc2 = tsc.io.read(f2)
-        cnt_tree1, cnt_tree2 = tsc.tl.consensus_run(sc1, sc2)
-        ii = nx.is_isomorphic(cnt_tree1, cnt_tree2)  # result in recomb.fig1c
-        assert ii == True
+        final_tree = tsc.tl.consensus_tree(sc1, sc2)
+        assert len(final_tree.nodes) == 21
 
     def test_consensus_3(self):
+        # result in biorxiv.fig4d
         f1 = tsc.ul.get_file("trisicell.datasets/test/consensus/biorxiv.fig4b.CFMatrix")
         f2 = tsc.ul.get_file("trisicell.datasets/test/consensus/biorxiv.fig4c.CFMatrix")
         sc1 = tsc.io.read(f1)
         sc2 = tsc.io.read(f2)
-        cnt_tree1, cnt_tree2 = tsc.tl.consensus_run(sc1, sc2)
-        ii = nx.is_isomorphic(cnt_tree1, cnt_tree2)  # result in biorxiv.fig4d
-        assert ii == True
+        final_tree = tsc.tl.consensus_tree(sc1, sc2)
+        assert len(final_tree.nodes) == 34
 
     def test_consensus_4(self):
+        # result in biorxiv.fig3f
         f1 = tsc.ul.get_file("trisicell.datasets/test/consensus/biorxiv.fig3b.CFMatrix")
         f2 = tsc.ul.get_file("trisicell.datasets/test/consensus/biorxiv.fig3c.CFMatrix")
         sc1 = tsc.io.read(f1)
         sc2 = tsc.io.read(f2)
-        cnt_tree1, cnt_tree2 = tsc.tl.consensus_run(sc1, sc2)
-        ii = nx.is_isomorphic(cnt_tree1, cnt_tree2)  # result in biorxiv.fig3f
-        assert ii == True
+        final_tree = tsc.tl.consensus_tree(sc1, sc2)
+        assert len(final_tree.nodes) == 19

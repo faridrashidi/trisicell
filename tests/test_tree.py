ca2e8f2b3cbaa90d7d500328438f596f2f83d472
Farid Rashidi <farid.rsh@gmail.com>
2021-04-09 21:13:16 -0400

    add test tree object builder

---
+++ tests/test_tree.py
@@ -0,0 +1,13 @@
+import pytest
+from _helpers import *
+
+import trisicell as tsc
+
+
+class TestTrees:
+    def test_to_tree(self):
+        file = tsc.ul.get_file("trisicell.datasets/test/fp_0-fn_0-na_0.ground.CFMatrix")
+        data = tsc.io.read(file)
+        tree = tsc.ul.to_tree(data)
+        assert len(list(tree.nodes)) == 10
+        assert len(list(tree.edges)) == 9

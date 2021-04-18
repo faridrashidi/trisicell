aaf62630f230fcb83baf09858bcac3e60f8daf5b
Farid Rashidi <farid.rsh@gmail.com>
2021-04-17 22:24:21 -0400

    Bump version: 0.0.3 → 0.0.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except ImportError:
     __author__ = ", ".join(["Farid Rashidi"])
     __maintainer__ = ", ".join(["Farid Rashidi"])
     __email__ = ", ".join(["farid.rsh@gmail.com"])
-    __version__ = "0.0.3"
+    __version__ = "0.0.4"
 
 extensions = [
     Extension(

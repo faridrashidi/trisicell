ee51a7c489f6760816199249f92af493bdcec1b4
Farid Rashidi <farid.rsh@gmail.com>
2021-05-28 00:10:11 -0400

    update installation script

--- .scripts/ci/install.sh
+++ .scripts/ci/install.sh
@@ -4,22 +4,15 @@ set -ev
 
 echo "Installing APT dependencies"
 if [[ "$OS" == "macos-latest" ]]; then
-  brew install gcc
   brew install R
   brew install graphviz
   pip install pygraphviz
   brew install graph-tool
-
-  python -m pip install --upgrade pip
-  pip install -e .
-  pip install pytest pytest-cov
+  brew install mpich
+  pip install mpi4py
 elif [[ "$OS" == "ubuntu-latest" ]]; then
-  python -m pip install --upgrade pip
-  pip install -e .
-  pip install pytest pytest-cov
-  # https://github.com/yarnpkg/yarn/issues/7866
   curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-  # R-related
+  
   sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
   sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
   sudo apt-get update -y
@@ -40,9 +33,16 @@ elif [[ "$OS" == "ubuntu-latest" ]]; then
   sudo apt-get install graphviz graphviz-dev -y
   pip install pygraphviz
 
+  sudo apt install libopenmpi-dev -y
+  pip install mpi4py
+
   # sudo add-apt-repository "deb [ arch=amd64 ] https://downloads.skewed.de/apt bullseye main"
   # sudo apt-key adv --keyserver keys.openpgp.org --recv-key 612DEFB798507F25
   # sudo apt-get install python3-graph-tool -y
 else
   exit 42
 fi
+
+python -m pip install --upgrade pip
+pip install -e .
+pip install pytest pytest-cov

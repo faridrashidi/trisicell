0252c5e25b333b5a279b79952de2eac28f7e29b0
Farid Rashidi <farid.rsh@gmail.com>
2021-09-22 18:35:30 -0400

    update

--- trisicell/tl/consensus/_consensus_day.py
+++ trisicell/tl/consensus/_consensus_day.py
@@ -21,22 +21,23 @@ def _add_artificial_edge(tree):
     artificial_added = []
     cp_tree = tree.copy()
     nodes = list(cp_tree.nodes)
-    max_node = max(nodes)
+    last_node = max(nodes)
     for n in nodes:
         if cp_tree.in_degree(n) == 1 and cp_tree.out_degree(n) > 0:
             if "––" not in cp_tree.nodes[n]["label"]:
                 for x in cp_tree.nodes[n]["label"]:
                     artificial_added.append(x)
-                    max_node += 1
-                    cp_tree.add_node(max_node, label=[x])
-                    cp_tree.add_edge(n, max_node)
+                    last_node += 1
+                    cp_tree.add_node(last_node, label=[x])
+                    cp_tree.add_edge(n, last_node)
                 cp_tree.nodes[n]["label"] = "––"
         elif cp_tree.in_degree(n) == 1 and cp_tree.out_degree(n) == 0:
             if len(cp_tree.nodes[n]["label"]) > 1:
                 for x in cp_tree.nodes[n]["label"]:
-                    max_node += 1
-                    cp_tree.add_node(max_node, label=[x])
-                    cp_tree.add_edge(n, max_node)
+                    artificial_added.append(x)
+                    last_node += 1
+                    cp_tree.add_node(last_node, label=[x])
+                    cp_tree.add_edge(n, last_node)
                 cp_tree.nodes[n]["label"] = "––"
     return cp_tree, artificial_added
 
@@ -74,11 +75,13 @@ def consensus_day(sc1, sc2):
     cnt_tree1 = _get_cnt_tree(tree1)
     cnt_tree2 = _get_cnt_tree(tree2)
 
+    # step 1
     cnt_tree1, artificial_added1 = _add_artificial_edge(cnt_tree1)
     cnt_tree2, artificial_added2 = _add_artificial_edge(cnt_tree2)
     artificial_added = list(np.union1d(artificial_added1, artificial_added2))
     tsc.logg.debug(artificial_added)
 
+    # step 2
     soin1 = _find_set_of_internal_nodes(cnt_tree1)
     soin2 = _find_set_of_internal_nodes(cnt_tree2)
 
@@ -91,8 +94,36 @@ def consensus_day(sc1, sc2):
     df = pd.DataFrame(0, index=indices, columns=columns, dtype=int)
     for i, inter in enumerate(intersection):
         df.loc[inter, f"m{i}"] = 1
-
     final_tree = tsc.ul.to_tree(df)
 
+    # step 3
+    final_tree = _get_cnt_tree(final_tree)
+    final_tree, _ = _add_artificial_edge(final_tree)
+
+    # step 4
+    nodes = list(final_tree.nodes)
+    for n in nodes:
+        if "––" not in final_tree.nodes[n]["label"] and final_tree.in_degree(n) == 1:
+            if final_tree.nodes[n]["label"][0] in artificial_added:
+                m = list(final_tree.predecessors(n))[0]
+                if "––" in final_tree.nodes[m]["label"]:
+                    final_tree.nodes[m]["label"] = final_tree.nodes[n]["label"]
+                else:
+                    final_tree.nodes[m]["label"] += final_tree.nodes[n]["label"]
+                final_tree = nx.contracted_nodes(final_tree, m, n, self_loops=False)
+
+    # convert back to the trisicell tree format
+    for v in final_tree.nodes:
+        if final_tree.in_degree(v) != 0:
+            if "––" not in final_tree.nodes[v]["label"]:
+                final_tree.nodes[v]["label"] = final_tree.graph["splitter_cell"].join(
+                    final_tree.nodes[v]["label"]
+                )
+    i = 0
+    for e, u, _ in final_tree.edges.data("label"):
+        final_tree.edges[(e, u)]["label"] = f"m{i}"
+        i += 1
+    data = tsc.ul.to_cfmatrix(final_tree)
+    final_tree = tsc.ul.to_tree(data)
+
     return final_tree
-    # return cnt_tree1, cnt_tree2

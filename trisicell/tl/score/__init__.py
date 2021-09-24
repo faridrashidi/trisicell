9061ea6a98122fc465e2e14dce3892d4b05c2e50
Farid Rashidi <farid.rsh@gmail.com>
2021-09-24 14:43:15 -0400

    update

--- trisicell/tl/score/__init__.py
+++ trisicell/tl/score/__init__.py
@@ -1,1 +1,2 @@
+from trisicell.tl.score._others import caset, disc
 from trisicell.tl.score._ours import ad, cc, dl, mltd, tpted
--- trisicell/tl/score/_others.py
+++ trisicell/tl/score/_others.py
@@ -1,3 +1,11 @@
+import itertools
+
+import networkx as nx
+import numpy as np
+
+import trisicell as tsc
+
+
 def bourque():
     # TODO: implement
     return None
@@ -8,11 +16,184 @@ def mp3():
     return None
 
 
-def disk():
-    # TODO: implement
-    return None
+def caset(df_grnd, df_sol):
+    """Measure of Common Ancestor Sets.
+
+    This measure was introduced in :cite:`CASet_DISC`.
+
+    Parameters
+    ----------
+    df_grnd : :class:`pandas.DataFrame`
+        The first genotype matrix (e.g. ground truth)
+        This matrix must be conflict-free.
+    df_sol : :class:`pandas.DataFrame`
+        The second genotype matrix (e.g. solution/inferred)
+        This matrix must be conflict-free.
+
+    Returns
+    -------
+    :obj:`float`
+        The score is out of 1.00
+    """
+
+    def _get_ancesteral_set(tree):
+        root = tsc.ul.root_id(tree)
+        ancesteral_set = {}
+        for n in tree.nodes:
+            if tree.in_degree(n) > 0 and "––" not in tree.nodes[n]["label"]:
+                for mut in tree.nodes[n]["label"]:
+                    ancester_set = ["root"]
+                    ancester_set += tree.nodes[n]["label"]  # self ancestor is ok
+                    for m in nx.shortest_path(tree, root, n):
+                        if tree.in_degree(m) > 0 and "––" not in tree.nodes[m]["label"]:
+                            ancester_set += tree.nodes[m]["label"]
+                    ancesteral_set[mut] = ancester_set
+        return ancesteral_set
+
+    def _get_common_ancesteral_set(muts, ancesteral_set):
+        common_ancesteral_set = {}
+        for x, y in itertools.combinations(muts, 2):
+            common_ancesteral_set[(x, y)] = np.intersect1d(
+                ancesteral_set[x], ancesteral_set[y]
+            )
+        return common_ancesteral_set
+
+    inter = np.intersect1d(df_grnd.columns, df_sol.columns)
+    if len(inter) == 0:
+        tsc.logg.error("No common mutations found between two trees!")
+    df_grnd1 = df_grnd[inter]
+    df_sol1 = df_sol[inter]
+
+    tree_grnd = tsc.ul.to_tree(df_grnd1)
+    tree_sol = tsc.ul.to_tree(df_sol1)
+    inter = np.setdiff1d(
+        inter,
+        np.union1d(
+            tree_grnd.graph["become_germline"], tree_sol.graph["become_germline"]
+        ),
+    )
+    df_grnd1 = df_grnd[inter]
+    df_sol1 = df_sol[inter]
+
+    tree_grnd = tsc.ul.to_tree(df_grnd1)
+    tree_sol = tsc.ul.to_tree(df_sol1)
+    tree_grnd = tsc.ul.to_mtree(tree_grnd)
+    tree_sol = tsc.ul.to_mtree(tree_sol)
+
+    ancesteral_set_grnd = _get_ancesteral_set(tree_grnd)
+    ancesteral_set_sol = _get_ancesteral_set(tree_sol)
+
+    common_ancesteral_set_grnd = _get_common_ancesteral_set(inter, ancesteral_set_grnd)
+    common_ancesteral_set_sol = _get_common_ancesteral_set(inter, ancesteral_set_sol)
+
+    final = []
+    for x, y in itertools.combinations(inter, 2):
+        a = len(
+            np.intersect1d(
+                common_ancesteral_set_grnd[(x, y)], common_ancesteral_set_sol[(x, y)]
+            )
+        )
+        b = len(
+            np.union1d(
+                common_ancesteral_set_grnd[(x, y)], common_ancesteral_set_sol[(x, y)]
+            )
+        )
+        final.append(a / b)
+
+    return np.mean(final)
 
 
-def caset():
-    # TODO: implement
-    return None
+def disc(df_grnd, df_sol):
+    """Measure of Distinctly Inherited Sets.
+
+    This measure was introduced in :cite:`CASet_DISC`.
+
+    Parameters
+    ----------
+    df_grnd : :class:`pandas.DataFrame`
+        The first genotype matrix (e.g. ground truth)
+        This matrix must be conflict-free.
+    df_sol : :class:`pandas.DataFrame`
+        The second genotype matrix (e.g. solution/inferred)
+        This matrix must be conflict-free.
+
+    Returns
+    -------
+    :obj:`float`
+        The score is out of 1.00
+    """
+
+    def _get_ancesteral_set(tree):
+        root = tsc.ul.root_id(tree)
+        ancesteral_set = {}
+        for n in tree.nodes:
+            if tree.in_degree(n) > 0 and "––" not in tree.nodes[n]["label"]:
+                for mut in tree.nodes[n]["label"]:
+                    ancester_set = ["root"]
+                    ancester_set += tree.nodes[n]["label"]  # self ancestor is ok
+                    for m in nx.shortest_path(tree, root, n):
+                        if tree.in_degree(m) > 0 and "––" not in tree.nodes[m]["label"]:
+                            ancester_set += tree.nodes[m]["label"]
+                    ancesteral_set[mut] = ancester_set
+        return ancesteral_set
+
+    def _get_distinctly_inherited_set(muts, ancesteral_set):
+        common_ancesteral_set = {}
+        for x, y in itertools.permutations(muts, 2):
+            common_ancesteral_set[(x, y)] = np.setdiff1d(
+                ancesteral_set[x], ancesteral_set[y]
+            )
+        return common_ancesteral_set
+
+    inter = np.intersect1d(df_grnd.columns, df_sol.columns)
+    if len(inter) == 0:
+        tsc.logg.error("No common mutations found between two trees!")
+    df_grnd1 = df_grnd[inter]
+    df_sol1 = df_sol[inter]
+
+    tree_grnd = tsc.ul.to_tree(df_grnd1)
+    tree_sol = tsc.ul.to_tree(df_sol1)
+    inter = np.setdiff1d(
+        inter,
+        np.union1d(
+            tree_grnd.graph["become_germline"], tree_sol.graph["become_germline"]
+        ),
+    )
+    df_grnd1 = df_grnd[inter]
+    df_sol1 = df_sol[inter]
+
+    tree_grnd = tsc.ul.to_tree(df_grnd1)
+    tree_sol = tsc.ul.to_tree(df_sol1)
+    tree_grnd = tsc.ul.to_mtree(tree_grnd)
+    tree_sol = tsc.ul.to_mtree(tree_sol)
+
+    ancesteral_set_grnd = _get_ancesteral_set(tree_grnd)
+    ancesteral_set_sol = _get_ancesteral_set(tree_sol)
+
+    distinctly_inherited_set_grnd = _get_distinctly_inherited_set(
+        inter, ancesteral_set_grnd
+    )
+    distinctly_inherited_set_sol = _get_distinctly_inherited_set(
+        inter, ancesteral_set_sol
+    )
+
+    final = []
+    for x, y in itertools.combinations(inter, 2):
+        a = len(
+            np.intersect1d(
+                distinctly_inherited_set_grnd[(x, y)],
+                distinctly_inherited_set_sol[(x, y)],
+            )
+        )
+        b = len(
+            np.union1d(
+                distinctly_inherited_set_grnd[(x, y)],
+                distinctly_inherited_set_sol[(x, y)],
+            )
+        )
+        if (
+            b > 0
+        ):  # FIXME: if a and b are in the same node distinctly_inherited_set is empty
+            final.append(a / b)
+
+    return np.mean(final)

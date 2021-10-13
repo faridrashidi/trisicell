d96bf53cc306e1ec5d1f4962d75d26489d8d2009
Farid Rashidi <farid.rsh@gmail.com>
2021-10-12 20:39:43 -0400

    update

--- trisicell/io/__init__.py
+++ trisicell/io/__init__.py
@@ -1,28 +1,10 @@
 """Input/Output Module."""
 
 from trisicell.io._genotype import read, write
-from trisicell.io._tmp import (
-    read_bamreadcount,
-    read_cnvkit,
-    read_defuse,
-    read_gatk,
-    read_rsem,
-    read_sc_bulk_simulation,
-    read_snpeff,
-    read_vep,
-)
 from trisicell.io._tree import to_png
 
 __all__ = (
-    read_defuse,
     read,
-    read_bamreadcount,
-    read_cnvkit,
-    read_gatk,
-    read_rsem,
-    read_sc_bulk_simulation,
-    read_snpeff,
-    read_vep,
     write,
     to_png,
 )

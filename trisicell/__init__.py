12b29bd60ecde5948ac6a0ef2048056c500596a0
Farid Rashidi <farid.rsh@gmail.com>
2021-11-22 21:05:09 -0500

    [skip ci] 0.0.19 → 0.0.20

--- trisicell/__init__.py
+++ trisicell/__init__.py
@@ -7,5 +7,5 @@ from trisicell import pl, pp, settings, tl, ul
 __author__ = ", ".join(["Farid Rashidi"])
 __maintainer__ = ", ".join(["Farid Rashidi"])
 __email__ = ", ".join(["farid.rsh@gmail.com"])
-__version__ = "0.0.19"
+__version__ = "0.0.20"
 __all__ = (datasets, io, logg, pl, pp, settings, tl, ul)
